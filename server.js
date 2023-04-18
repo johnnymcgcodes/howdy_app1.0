ba87e5408ddc9943e672d721eeffd5f82cd485ac
Johnny McG <johnnymcg@outlook.com>
2023-04-18 13:59:28 -0700

    seperated api routes

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const {PORT, mongoUri} = require('./config')
 const cors = require('cors')
 const morgan = require("morgan");
 const bodyParser = require('body-parser')
-// const bucketListItemRoutes = require('./routes/api/bucketListItems')
 const userRoutes = require('./routes/api/users')
 app.use(cors())
 app.use(morgan('tiny'));
@@ -28,7 +27,7 @@ app.use('/api/users', userRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
